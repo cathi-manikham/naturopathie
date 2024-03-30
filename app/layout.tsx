b8c10b734066a11081618d0bfbe8601d9e2d8f3e
loogark <ragoolkrishnan.ram@gmail.com>
2024-03-30 03:32:39 +0100

    Update metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,21 @@ import Header from "../components/Header";
 import { StyleProviders } from "../providers/chakra.provider";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://naturopathie.vercel.app/"),
   title: "Catherine Manikham | Naturopathe",
-  description:
-    "Embarquez pour un voyage de guérison naturelle avec Catherine Manikham",
+  description: ` Le naturopathe utilise des thérapies naturelles pour promouvoir la
+    santé et le bien-être, en identifiant et en traitant les causes
+    profondes des maladies.`,
+  keywords:
+    "naturopathie, Approche holistique, Médecines naturelles, Soins individualisés,Catherine Manikham, Catherine, Manikham, bien-être, guérison naturelle",
+  openGraph: {
+    title: "Accueil - Catherine Manikham | Naturopathe",
+    type: "website",
+    description: `Le naturopathe utilise des thérapies naturelles pour promouvoir la
+      santé et le bien-être, en identifiant et en traitant les causes
+      profondes des maladies.`,
+    images: "/home.png",
+  },
 };
 
 export default function RootLayout({

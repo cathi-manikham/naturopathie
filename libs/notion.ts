16780d44292adba3b36767d223487838e87be004
loogark <ragoolkrishnan.ram@gmail.com>
2024-03-30 19:23:49 +0100

    Refactor code and update ChakraProvider import

--- libs/notion.ts
+++ libs/notion.ts
@@ -1,7 +1,6 @@
 import { Client } from "@notionhq/client";
 import { NotionToMarkdown } from "notion-to-md";
 import { NotionArticle } from "../types/notion";
-
 const notion = new Client({
   auth: process.env.NEXT_PUBLIC_NOTION_TOKEN,
 });
--- providers/chakra.provider.tsx
+++ providers/chakra.provider.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { theme } from "../theme/theme";
 
 export function StyleProviders({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
+    <>
       <ColorModeScript initialColorMode='light' />
       <ChakraProvider theme={theme}>{children}</ChakraProvider>
-    </CacheProvider>
+    </>
   );
 }

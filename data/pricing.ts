e1b6a6f8590c92513832d8a2f53ab905a2399e51
Yohan <yohan.thomas27@gmail.com>
2025-03-13 16:34:12 +0100

    add text

--- data/pricing.ts
+++ data/pricing.ts
@@ -1,57 +1,57 @@
 export const pricing = [
-  {
-    id: 1,
-    title: "Bilan de vitalité (1h30)",
-    price: "80 Euros",
-    description:
-      "Séance de naturopathie incluant de la morphologie, de l’iridologie, et d’un échange sur votre mode de vie. Le tout débouchant sur la proposition d’une cure personnalisée.",
-    features: [
-      { id: 1, desc: "Consultation à domicile", isValid: true },
-      {
-        id: 2,
-        desc: "Consultation en visioconférence",
-        isValid: true,
-      },
-      {
-        id: 3,
-        desc: "Paiement par virement bancaire, chèque, ou espèces",
-        isValid: true,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Séance de suivi (1h)",
-    price: "80 Euros",
-    description:
-      "Séance de naturopathie qui consiste à échanger sur les actions que vous avez pu mettre en place suite à la première séance. Elle se termine par la proposition d’une cure complémentaire répondant à votre objectif.",
-    features: [
-      { id: 1, desc: "Consultation à domicile", isValid: true },
-      {
-        id: 2,
-        desc: "Consultation en visioconférence",
-        isValid: true,
-      },
-      {
-        id: 3,
-        desc: "Paiement par virement bancaire, chèque, ou espèces",
-        isValid: true,
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "Massages",
-    price: "90 Euros",
-    description: "• Massage Détox\n• Massage Relaxant\n• Massage à domicile",
-    features: [
-      { id: 1, desc: "À domicile", isValid: true },
-      {
-        id: 2,
-        desc: "Paiement par virement bancaire, chèque, ou espèces",
-        isValid: true,
-      },
-    ],
-  },
+	{
+		id: 1,
+		title: "Bilan de vitalité (1h30)",
+		price: "80 Euros",
+		description:
+			"Séance de naturopathie incluant de la morphologie, de l’iridologie, et d’un échange sur votre mode de vie. Le tout débouchant sur la proposition d’une cure personnalisée.",
+		features: [
+			{ id: 1, desc: "Consultation à domicile", isValid: true },
+			{
+				id: 2,
+				desc: "Consultation en visioconférence",
+				isValid: true,
+			},
+			{
+				id: 3,
+				desc: "Paiement par virement bancaire, chèque, ou espèces",
+				isValid: true,
+			},
+		],
+	},
+	{
+		id: 2,
+		title: "Séance de suivi (1h00)",
+		price: "80 Euros",
+		description:
+			"Séance de naturopathie qui consiste à échanger sur les actions que vous avez pu mettre en place suite à la première séance. Elle se termine par la proposition d’une cure complémentaire répondant à votre objectif.",
+		features: [
+			{ id: 1, desc: "Consultation à domicile", isValid: true },
+			{
+				id: 2,
+				desc: "Consultation en visioconférence",
+				isValid: true,
+			},
+			{
+				id: 3,
+				desc: "Paiement par virement bancaire, chèque, ou espèces",
+				isValid: true,
+			},
+		],
+	},
+	{
+		id: 3,
+		title: "Massages (1h00)",
+		price: "90 Euros",
+		description: "• Massage Détox\n• Massage Relaxant\n• Drainage manuel bien-être\n• Chi Nei Tsang",
+		features: [
+			{ id: 1, desc: "À domicile", isValid: true },
+			{
+				id: 2,
+				desc: "Paiement par virement bancaire, chèque, ou espèces",
+				isValid: true,
+			},
+		],
+	},
 
 ];
